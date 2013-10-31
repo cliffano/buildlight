369473cc0430697fe6e01e2932a73de7fd809657
Cliffano Subagio <cliffano@gmail.com>
2013-11-01 08:42:37 +1100

    Add buster-based integration test.

---
+++ test-integration/buildlight.js
@@ -0,0 +1,15 @@
+var BuildLight = require('../lib/buildlight'),
+  buster = require('buster-node'),
+  referee = require('referee'),
+  assert = referee.assert;
+
+buster.testCase('buildlight - buildlight', {
+  'should throw error when platform is not supported or usbled is not available, otherwise return buildlight': function () {
+    try {
+      var buildLight = new BuildLight();
+      assert.defined(buildLight);
+    } catch (err) {
+      assert.isTrue(err.message.match(/[Unsupported platform | Unable to find USB LED driver installation]/) !== undefined);
+    }
+  }
+});
