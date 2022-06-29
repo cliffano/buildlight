5762b857b4a9b742a043b4ee45bcb2870a2afa95
cliffano <cliffano@users.noreply.github.com>
2022-06-29 01:54:52 +0000

    deploy: 484f82e9378323bf5693b7b1d506e638f2b530f0

--- complexity/plato/files/lib_buildlight_js/report.history.js
+++ complexity/plato/files/lib_buildlight_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Wed, 29 Jun 2022 00:41:46 GMT","sloc":146,"lloc":48,"functions":14,"deliveredBugs":0.617,"difficulty":34.941,"maintainability":76.561,"lintErrors":5}]
+__history = [{"date":"Wed, 29 Jun 2022 01:54:46 GMT","sloc":146,"lloc":48,"functions":14,"deliveredBugs":0.615,"difficulty":33.353,"maintainability":76.561,"lintErrors":5}]
